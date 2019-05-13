864b2c8c1bf88a26c1031b81e1c37dc94f6e96e2
ken1286 <kindle1286@gmail.com>
2019-05-13 15:48:33 -0700

    Fixed count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,6 +14,7 @@ class Counter extends Component {
     };
 
     render() {
+        console.log(this.props.count)
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
